4f908c6bb62f3b0403fb03cf44842993ebd796b2
Kestutis1 <djkestutis@yahoo.com>
2019-01-09 15:32:13 +0200

    Pradedu prekių atvaizdavimą

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
-      include('header.php');
-      include('db.php');
-      include('footer.php');
+  include('header.php');
+  include('db.php');
+
+    $dispaly_block = "<h1>Prekių kategorijos</h1>
+    <p>Pasirinkite kategoriją pamatytį jos prekes</p>";
+
+    // IDEA: Kategorijų rodymas
+    $get_cats_sql = "SELECT id, kat_pavadinimas, kat_aprašymas FROM
+                      prekiu_kategorijos ORDER BY kat_pavadinimas";
+    $get_cats_res = mysqli_query(getPrisijungimas(), $get_cats_sql)
+                      or die(mysqli_error(getPrisijungimas()));
+
+if (mysqli_num_rows($get_cats_res) < 1) {
+    $dispaly_block = "<p>Atsiprašome nėra kategorijų peržiūrėti</p>";
+} else {
+    while ($cats = mysqli_fetch_array($get_cats_res)) {
+      $cat_id = $cats['id'];
+      $cat_title = strtoupper(stripslashes($cats['kat_pavadinimas']));
+      $cat_desc = stripslashes($cats['kat_aprašymas']);
+
+      $dispaly_block .= "<p><strong><a href=\"".$_SERVER["PHP_SELF"].
+      "?cat_id=".$cat_id."\">".$cat_title."</a></strong><br />"
+      .$cat_desc."</p>";
+
+
+    }
+}
+
+echo $dispaly_block;
+
  ?>
+<?php include('footer.php'); ?>
--- prekiu_valdymas.php
+++ prekiu_valdymas.php
@@ -50,8 +50,6 @@
 
       // IDEA: Susitvarkom kintamuosius.
             $katId = mysqli_real_escape_string(getPrisijungimas(), $_GET['kategorija']);
-            // $spalva = mysqli_real_escape_string(getPrisijungimas(), $_GET['spalva']);
-            // $dydis = mysqli_real_escape_string(getPrisijungimas(), $_GET['dydis']);
             $prePavadinimas = mysqli_real_escape_string(getPrisijungimas(), $_GET['prePavadinimas']);
             $preKaina = mysqli_real_escape_string(getPrisijungimas(), $_GET['preKaina']);
             $preAprašymas = mysqli_real_escape_string(getPrisijungimas(), $_GET['aprašymas']);
@@ -62,16 +60,10 @@
           $SQL = "INSERT INTO `prekes` (`id`, `kat_id`, `prekės_pavadinimas`, `prekės_kaina`, `prekės_aprašymas`)
                   VALUES (NULL, '$katId', '$prePavadinimas', '$preKaina', '$preAprašymas');";
 
-          // $SQLdydis = "INSERT INTO `prekiu_dydis` (`prekės_id`, `prekės_dydis`)
-          //         VALUES (NULL, '$dydis');";
-          // $SQLspalva = "INSERT INTO `prekiu_spalvos` (`prekės_id`, `prekės_spalva`)
-          //               VALUES (NULL, '$spalva');";
+
           $ikeliam = mysqli_query(getPrisijungimas(), $SQL);
           $last_id = mysqli_insert_id(getPrisijungimas());
 
-
-          // $ikeliamSpalva = mysqli_query(getPrisijungimas(), $SQLspalva);
-          // $ikeliamDydi = mysqli_query(getPrisijungimas(), $SQLdydis);
 
               echo "<h3>Jūsų prekė sėkmingai įkelta</h3>";
               header("Location: prekiu_valdymas.php?id=$last_id");
